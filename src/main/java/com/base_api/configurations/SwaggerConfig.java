f061d4d8d46be58be19cbb8f239e3cb25b2b328b
Pablo Gorgoglione <pablogorgoglione@gmail.com>
2025-03-05 18:05:17 -0300

    update swagger config

--- src/main/java/com/base_api/configurations/SwaggerConfig.java
+++ src/main/java/com/base_api/configurations/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.base_api.configurations;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,8 +17,17 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("DUX SOFTWARE | PRUEBA TÉCNICA ")
                         .version("1.0.0")
-                        .description("documentación para la prueba técnica de DUX SOFTWARE")
-                        .contact(new io.swagger.v3.oas.models.info.Contact().name("Support").email("support@example.com"))
-                        .license(new io.swagger.v3.oas.models.info.License().name("Apache 2.0").url("http://springdoc.org")));
+                        .description("Documentación para la prueba técnica de DUX SOFTWARE")
+                        .contact(new io.swagger.v3.oas.models.info.Contact().name("Support").email(
+                                "support@example.com"))
+                        .license(new io.swagger.v3.oas.models.info.License().name("Apache 2.0").url(
+                                "http://springdoc.org")))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"))
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
